87a06d7c4b19576debcad641d5eb9e8d3f0e6c87
SaToGo <stoledano97@gmail.com>
2022-02-11 13:34:13 +0100

    function for computer hand choice

--- game.js
+++ game.js
@@ -1,4 +1,14 @@
-function computerPlay () 
-{
-    return 0;    
+function computerPlay () {
+
+    let handType = "Rock";
+    //random number between 1 and 3.    
+    let randomNumber = Math.floor( Math.random() * (4 - 1)) + 1;
+
+    console.log("random = " + randomNumber);
+    if (randomNumber == 1) handType = 'Rock';
+    else if (randomNumber == 2) handType = 'Paper';
+    else if (randomNumber == 3) handType = 'Scissors';
+    else console.log("error at computerPlay function");
+
+    return handType;    
 }
