d7b21e842a761aeb00071e15aa1d9fbd4583da3a
SaToGo <stoledano97@gmail.com>
2022-03-10 23:36:00 +0100

    function updateWinsLoses for the best of 5 rounds added and wins loses variable added

--- game.js
+++ game.js
@@ -18,79 +18,69 @@ gets (playerSelection, computerSelection)
     "rock"
     "paper"
     "scissors" 
-
-returns result -> number that represents victory deafeat or draw
-    0-> Draw,
-    1-> Victory,
-    2-> Lose,
    **************************************  */
 function playRound(playerSelection, computerSelection){
 
-    let result = 0;
     const resultDom = document.querySelector("#result");
     if (playerSelection == computerSelection){
         resultDom.textContent = "Draw! your opponent also pick " + playerSelection;
-        result = 0;
     }
     else if (playerSelection == "rock"){
         if (computerSelection == "paper") {
             resultDom.textContent = "You Lose! Paper beats Rock";
-            result = 2;
+            loses = loses + 1;
         }
         else {
             resultDom.textContent = "You Win! Rock beats Scissors";
-            result = 1;
+            wins = wins + 1;
         }
     }
     else if (playerSelection == "paper"){
         if (computerSelection == "scissors") {
             resultDom.textContent = "You Lose! Scissors beats Paper";
-            result = 2;
+            loses = loses + 1;
         }
         else {
             resultDom.textContent = "You Win! Paper beats Rock";
-            result = 1;
+            wins = wins + 1;
         }
     }
     else if (playerSelection == "scissors"){
         if (computerSelection == "rock") {
             resultDom.textContent = "You Lose! Rock beats Scissors";
-            result = 2;
+            loses = loses + 1;
         }
         else {
             resultDom.textContent = "You Win! Scissors beats Paper";
-            result = 1;
+            wins = wins + 1;
         }
     } 
-    return result;
+    updateWinsLoses();
 }
 
-/*
-function game(){
+function updateWinsLoses(){
+    const winsDOM = document.querySelector("#wins");
+    const losesDOM = document.querySelector("#loses");
 
-    let playerSelection = "empty"
-    let computerSelection = "empty"
+    checkWinner();
+    winsDOM.textContent = "wins: " + wins;
+    losesDOM.textContent = "loses: " + loses;
 
-    playerSelection = prompt("Select Rock, Paper or Scissors").toLowerCase();
-    computerWins = 0;
-    computerSelection = computerPlay();
+}
 
-    let result = playRound(playerSelection, computerSelection);
-    if (result == 1){
-        playerWins++;
+function checkWinner(){
+    const matchWinner = document.querySelector("#MatchWinner")
+    matchWinner.textContent = ""
+    if(wins == 5){
+        matchWinner.textContent = "You Are The Winner with " + wins + " wins and " + loses + " loses";
+        wins = 0;
+        loses = 0;
     }
-    else if(result == 2){
-        computerWins++;
-    }
-
-    if (computerWins > playerWins){
-        console.log("You lost the best of Five rounds.");
-    }
-    else if (computerWins < playerWins){
-        console.log("You win the best of Five rounds.");
-    }
-    else {
-        console.log("Draw in a best of Five rounds.");
+    else if(loses == 5){
+        matchWinner.textContent = "You Lost, Computer is the Winner you had " + wins + " wins and " + loses + " loses";
+        wins = 0;
+        loses = 0;
     }
 }
-*/
+let wins = 0;
+let loses = 0;
